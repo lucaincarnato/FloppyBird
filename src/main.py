51c9660318d0a78ce7dd9c6156d3da2e374db281
lucaincarnato <lucaincarnato.03@gmail.com>
2022-08-23 15:14:43 +0200

    Player initialization and jumping-gravity

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import pygame 
 from sys import exit
+from player import Floppy
 
 pygame.init()
 screen = pygame.display.set_mode((400,700))
@@ -8,6 +9,10 @@ clock = pygame.time.Clock()
 # Sky and Ground surface
 sky = pygame.image.load('Assets/sky.png').convert()
 ground = pygame.image.load('Assets/ground.png').convert()
+
+# Initializing the floppy Sprite Group
+floppy = pygame.sprite.GroupSingle()
+floppy.add(Floppy())
 
 while True:
     for event in pygame.event.get():
@@ -18,6 +23,10 @@ while True:
     # Sky and Ground on screen
     screen.blit(sky,(0,0))
     screen.blit(ground,(0,600))
+
+    # Drawing and updating the Floppy on the disk
+    floppy.draw(screen)
+    floppy.update()
 
     pygame.display.update()
     clock.tick(60)
---
+++ src/player.py
@@ -0,0 +1,48 @@
+import pygame
+
+class Floppy(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+
+        # Importing all the images and store them in a list to pick them gradually and make an animation
+        floppy1 = pygame.image.load('Assets/floppy/floppy1.png').convert_alpha()
+        floppy2 = pygame.image.load('Assets/floppy/floppy2.png').convert_alpha()
+        floppy3 = pygame.image.load('Assets/floppy/floppy3.png').convert_alpha()
+        floppy4 = pygame.image.load('Assets/floppy/floppy4.png').convert_alpha()
+        self.floppy = [floppy1,floppy2,floppy3,floppy4]
+
+        # Indexes for the animation and gravity
+        self.floppy_index = 0
+        self.gravity = 0
+
+        self.image = self.floppy[self.floppy_index]
+        self.rect = self.image.get_rect(center = (100,300))
+
+    # Method to enable jumping and gravity
+    def motion(self):
+        # If mouse pressed floppy jumps
+        if pygame.mouse.get_pressed()[0] and self.rect.bottom <= 600:
+            self.gravity = -3
+        
+        # The gravity will increment at each update, changing the y pos of the flappy and falling faster the more we fall
+        self.gravity += 0.3
+        self.rect.y += self.gravity
+        # If floppy touches ground it stops
+        if self.rect.bottom >= 600:
+            self.rect.bottom = 600
+        # If floppy touches ceiling it stops
+        elif self.rect.top <= 0:
+            self.rect.top = 0
+
+    # Method to update the images of the surface to make the motion animation
+    def animation(self):
+        self.floppy_index += 0.1
+        if self.floppy_index >= len(self.floppy):
+            self.floppy_index = 0
+        self.image = self.floppy[int(self.floppy_index)]
+        
+
+    def update(self):
+        self.animation()
+        self.motion()
+
